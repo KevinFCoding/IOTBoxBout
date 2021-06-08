1c321b5e23a64641127f0b3d061e93a85404edc7
Mélanie <melanie.vaubien@hotmail.com>
2021-06-08 17:12:21 +0200

    Components

---
+++ RNApp/FlowerApp/components/CustomisableButton/customisableButton.style.js
@@ -0,0 +1,14 @@
+import { StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+    actionButton:{
+        backgroundColor:"#FFE075",
+        padding:10,
+        borderRadius:10
+    },
+    actionButtonText:{
+        color: "black",
+        textAlign:'center',
+        fontSize:20
+    }
+})
