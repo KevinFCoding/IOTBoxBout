4808b0af5c647ddb05ddd033acf15df0e80dd5d6
Max <maximejoullin@gmail.com>
2021-06-09 09:22:11 +0200

    add connexion db

--- Commandes/index.js
+++ Commandes/index.js
@@ -1,7 +1,47 @@
 const http = require('http');
+var firebase = require("firebase");
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDNEHFXG8rRQdXxLOGhP8uyVcBVgiq53lA",
+  authDomain: "box-bout.firebaseapp.com",
+  databaseURL: "https://box-bout-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "box-bout",
+  storageBucket: "box-bout.appspot.com",
+  messagingSenderId: "187684869556",
+  appId: "1:187684869556:web:55478cc6422b19e1434ee1",
+});
+
+var db = firebase.database();
+
+console.log(db);
+
+var testData = {
+  waterLevel: 55,
+  puceID: 3
+}
+
+// write data into RTDB
+db.ref("puces").set(testData, function(error) {
+  if (error) {
+    // The write failed...
+    console.log("Failed with error: " + error)
+  } else {
+    // The write was successful...
+    console.log("success")
+  }
+})
+
+// read data from RTDB
+database.ref('puces').once('value')
+.then(function(snapshot) {
+    console.log( snapshot.val() )
+})
+
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
